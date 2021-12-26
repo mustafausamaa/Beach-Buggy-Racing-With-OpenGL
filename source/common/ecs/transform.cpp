66fb6d734c0686b79814b4ae2f022dd2de38628f
Mustafa Usama <mustafausamaa@gmail.com>
2021-12-26 21:12:24 +0200

    Phase2

---
+++ source/common/ecs/transform.cpp
@@ -0,0 +1,26 @@
+#include "entity.hpp"
+#include "../deserialize-utils.hpp"
+
+#include <glm/gtx/euler_angles.hpp>
+namespace our {
+
+    // This function computes and returns a matrix that represents this transform
+    // Remember that the order of transformations is: Scaling, Rotation then Translation
+    // HINT: to convert euler angles to a rotation matrix, you can use glm::yawPitchRoll
+    glm::mat4 Transform::toMat4() const {
+        //TODO: Write this function
+        glm::mat4 ScaleMatrix = glm::scale(glm::mat4(1.0f), scale);
+        glm::mat4 TranslateMatrix = glm::translate(glm::mat4(1.0f), position);
+        glm::mat4 RotaionMatrix = glm::yawPitchRoll(rotation.y, rotation.x, rotation.z);
+        glm::mat4 TransformationMatrix=TranslateMatrix*RotaionMatrix*ScaleMatrix; 
+        return  TransformationMatrix; 
+    }
+
+     // Deserializes the entity data and components from a json object
+    void Transform::deserialize(const nlohmann::json& data){
+        position = data.value("position", position);
+        rotation = glm::radians(data.value("rotation", glm::degrees(rotation)));
+        scale    = data.value("scale", scale);
+    }
+
+}
---
+++ source/common/material/material.cpp
@@ -0,0 +1,90 @@
+#include "material.hpp"
+
+#include "../asset-loader.hpp"
+#include "deserialize-utils.hpp"
+
+namespace our
+{
+
+    // This function should setup the pipeline state and set the shader to be used
+    void Material::setup() const
+    {
+        // TODO: Write this function
+
+        // 1) set the pipeline state and set the shader to be used
+        pipelineState.setup();
+        shader->use();
+    }
+
+    // This function read the material data from a json object
+    void Material::deserialize(const nlohmann::json &data)
+    {
+        if (!data.is_object())
+            return;
+
+        if (data.contains("pipelineState"))
+        {
+            pipelineState.deserialize(data["pipelineState"]);
+        }
+        shader = AssetLoader<ShaderProgram>::get(data["shader"].get<std::string>());
+        transparent = data.value("transparent", false);
+    }
+
+    // This function should call the setup of its parent and
+    // set the "tint" uniform to the value in the member variable tint
+    void TintedMaterial::setup() const
+    {
+        // TODO: Write this function
+
+        // 1) call material setup to set the pipeline state and set the shader to be used
+        Material::setup();
+
+        // 2) set the Uniform tint in tinted shader with the data member tint
+        shader->set("tint", tint);
+    }
+
+    // This function read the material data from a json object
+    void TintedMaterial::deserialize(const nlohmann::json &data)
+    {
+        Material::deserialize(data);
+        if (!data.is_object())
+            return;
+        tint = data.value("tint", glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
+    }
+
+    // This function should call the setup of its parent and
+    // set the "alphaThreshold" uniform to the value in the member variable alphaThreshold
+    // Then it should bind the texture and sampler to a texture unit and send the unit number to the uniform variable "tex"
+    void TexturedMaterial::setup() const
+    {
+        // TODO: Write this function
+
+        // 1) call material setup to set the pipeline state and set the shader to be used
+        Material::setup();
+
+        // 2) set the Uniform alphaThreshold in textured shader with the data member alphathreshold
+        shader->set("alphaThreshold", alphaThreshold);
+
+        // 3) set the active texture unit to 0 then bind the texture to it
+        glActiveTexture(GL_TEXTURE0);
+        texture->bind();
+
+        // 4) bind the sampler to the same unit number 0
+        sampler->bind(0);
+
+        // 5) we send 0(the index of the texture unit we used above)to the "tex" uniform in textured shader
+        shader->set("tex", 0);
+    }
+
+    // This function read the material data from a json object
+    void TexturedMaterial::deserialize(const nlohmann::json &data)
+    {
+        TintedMaterial::deserialize(data);
+        if (!data.is_object())
+            return;
+        alphaThreshold = data.value("alphaThreshold", 0.0f);
+        texture = AssetLoader<Texture2D>::get(data.value("texture", ""));
+        sampler = AssetLoader<Sampler>::get(data.value("sampler", ""));
+    }
+
+}
