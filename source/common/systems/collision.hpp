8d062b4129a812e2726e220d300abcdb1980be4b
abdallah808 <abdallahahmedfawzy@outlook.com>
2022-01-09 04:17:46 +0200

    new

--- source/common/systems/collision.hpp
+++ source/common/systems/collision.hpp
@@ -5,6 +5,7 @@
 #include "../components/movement.hpp"
 #include "../components/free-camera-controller.hpp"
 #include "../components/camera.hpp"
+#include "../components/mesh-renderer.hpp"
 #include <glm/glm.hpp>
 #include <glm/gtc/constants.hpp>
 #include <glm/trigonometric.hpp>
@@ -100,35 +101,121 @@ namespace our
                                         {
                                             CameraControllerEntity->localTransform.position.x++;
                                             MovementComponent *speed = collidingEntity->parent->getComponent<MovementComponent>();
-                                            speed->linearVelocity.z--;
+                                            speed->linearVelocity.z += deltaTime;
                                         }
                                         // if the camera controlling our motorcycle is in the left side of the collision then collide and go left and collidingwith object go right
                                         else if (CameraControllerEntity->localTransform.position.x < collideWithEntity->localTransform.position.x)
                                         {
                                             CameraControllerEntity->localTransform.position.x--;
                                             MovementComponent *speed = collidingEntity->parent->getComponent<MovementComponent>();
-                                            speed->linearVelocity.z--;
+                                            speed->linearVelocity.z += deltaTime;
                                         }
                                     }
                                     // if  Enemy  collide with the Concrete barriers
                                     // then check which is right and which is left to make the collision left and right in logical view and decrease the moto speed
                                     else if (collidingEntity->name == "EnemyMoto" && collideWithEntity->name == "ConcreteBarrier")
                                     {
-                                    
 
                                         if (collidingEntity->localTransform.position.x >= collideWithEntity->localTransform.position.x)
 
                                         {
                                             collidingEntity->localTransform.position.x++;
                                             MovementComponent *speed = collidingEntity->getComponent<MovementComponent>();
-                                            speed->linearVelocity.z--;
+                                            speed->linearVelocity.z += deltaTime;
                                         }
                                         // if the camera controlling our motorcycle is in the left side of the collision then collide and go left and collidingwith object go right
                                         else if (collidingEntity->localTransform.position.x < collideWithEntity->localTransform.position.x)
                                         {
                                             collidingEntity->localTransform.position.x--;
                                             MovementComponent *speed = collidingEntity->getComponent<MovementComponent>();
-                                            speed->linearVelocity.z--;
+                                            speed->linearVelocity.z += deltaTime;
+                                        }
+                                    }
+                                    // if  MyMoto  collide with the red barriers
+                                    // then check which is right and which is left to make the collision left and right in logical view and decrease the moto speed
+                                    else if (collidingEntity->name == "MyMoto" && collideWithEntity->name == "RedBarrier")
+                                    {
+                                        our::Entity *CameraControllerEntity = collidingEntity->parent;
+
+                                        if (CameraControllerEntity->localTransform.position.z >= collideWithEntity->localTransform.position.z)
+
+                                        {
+                                            CameraControllerEntity->localTransform.position.z += 2;
+                                            MovementComponent *speed = collidingEntity->parent->getComponent<MovementComponent>();
+                                            speed->linearVelocity.z += deltaTime;
+                                        }
+                                        // if the camera controlling our motorcycle is in the left side of the collision then collide and go left and collidingwith object go right
+                                        else if (CameraControllerEntity->localTransform.position.z < collideWithEntity->localTransform.position.z)
+                                        {
+                                            CameraControllerEntity->localTransform.position.z += 2;
+                                            MovementComponent *speed = collidingEntity->parent->getComponent<MovementComponent>();
+                                            speed->linearVelocity.z += deltaTime;
+                                        }
+                                    }
+                                    // if  Enemy  collide with the red barriers
+                                    // then check which is right and which is left to make the collision left and right in logical view and decrease the moto speed
+                                    else if (collidingEntity->name == "EnemyMoto" && collideWithEntity->name == "RedBarrier")
+                                    {
+
+                                        if (collidingEntity->localTransform.position.z >= collideWithEntity->localTransform.position.z)
+
+                                        {
+                                            collidingEntity->localTransform.position.z += 2;
+                                            MovementComponent *speed = collidingEntity->getComponent<MovementComponent>();
+                                            speed->linearVelocity.z += deltaTime;
+                                        }
+                                        // if the camera controlling our motorcycle is in the left side of the collision then collide and go left and collidingwith object go right
+                                        else if (collidingEntity->localTransform.position.z < collideWithEntity->localTransform.position.z)
+                                        {
+                                            collidingEntity->localTransform.position.z += -2;
+                                            MovementComponent *speed = collidingEntity->getComponent<MovementComponent>();
+                                            speed->linearVelocity.z += deltaTime;
+                                        }
+                                    }
+                                    else if (collidingEntity->name == "MyMoto" && collideWithEntity->name == "Rocket")
+                                    {
+                                        our::Entity *CameraControllerEntity = collidingEntity->parent;
+
+                                        if (CameraControllerEntity->localTransform.position.z >= collideWithEntity->localTransform.position.z)
+
+                                        {
+
+                                            MovementComponent *speed = collidingEntity->parent->getComponent<MovementComponent>();
+                                            speed->linearVelocity.z += -0.5; // speed increase by 1 in -z direction
+                                            MeshRendererComponent *rocket = collideWithEntity->getComponent<MeshRendererComponent>();
+
+                                            if (rocket)
+                                            {
+                                                std::cout << "a7a" << std::endl;
+                                                collideWithEntity->deleteComponent<MeshRendererComponent>();
+                                            }
+                                        }
+                                        // if the camera controlling our motorcycle is in the left side of the collision then collide and go left and collidingwith object go right
+                                        else if (CameraControllerEntity->localTransform.position.z < collideWithEntity->localTransform.position.z)
+                                        {
+                                            MovementComponent *speed = collidingEntity->parent->getComponent<MovementComponent>();
+                                            speed->linearVelocity.z += -0.5; // speed increase by 1 in -z direction
+                                            collideWithEntity->deleteComponent<MeshRendererComponent>();
+                                        }
+                                    }
+                                    // if  Enemy  collide with the red barriers
+                                    // then check which is right and which is left to make the collision left and right in logical view and decrease the moto speed
+                                    else if (collidingEntity->name == "EnemyMoto" && collideWithEntity->name == "Rocket")
+                                    {
+
+                                        if (collidingEntity->localTransform.position.z >= collideWithEntity->localTransform.position.z)
+
+                                        {
+                                            MovementComponent *speed = collidingEntity->getComponent<MovementComponent>();
+                                            speed->linearVelocity.z += -0.5; // speed increase by 1 in -z direction
+                                            collideWithEntity->deleteComponent<MeshRendererComponent>();
+                                        }
+                                        // if the camera controlling our motorcycle is in the left side of the collision then collide and go left and collidingwith object go right
+                                        else if (collidingEntity->localTransform.position.z < collideWithEntity->localTransform.position.z)
+                                        {
+                                            MovementComponent *speed = collidingEntity->getComponent<MovementComponent>();
+                                            speed->linearVelocity.z += -0.5; // speed increase by 1 in -z direction
+                                            collideWithEntity->deleteComponent<MeshRendererComponent>();
                                         }
                                     }
                                 }
